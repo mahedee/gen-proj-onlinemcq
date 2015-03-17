37e324aae63a36b35414a8d3773e1cb08fb3f54b
asif007 <ibnaasif@gmail.com>
2015-03-18 00:52:56 +0600

    commit

--- OnlineMcq/OnlineMcq/Controllers/HomeController.cs
+++ OnlineMcq/OnlineMcq/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace OnlineMcq.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "Modify ";
 
             return View();
         }
